29dec7334101af43110cc4cf6476df5d79aace62
Miguel Aréjula Aísa <arejula10@gmail.com>
2025-05-05 12:10:48 +0200

    Refactor API routing and update Swagger server URL to HTTPS

---
+++ app/routes/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const indexRouter = require('./index');
+const personRouter = require('./people');
+const authRouter = require('./auth');
+const spaceRouter = require('./space');
+const reservationsRouter = require('./reservations');
+const notificationRouter = require('./notifications');
+const buildingRouter = require('./buildings');
+const docsRoute = require('./docs');
+
+router.use('/', indexRouter);
+router.use('/people', personRouter);
+router.use('/login', authRouter);
+router.use('/spaces', spaceRouter);
+router.use('/reservations', reservationsRouter);
+router.use('/notifications', notificationRouter);
+router.use('/buildings', buildingRouter);
+router.use('/docs', docsRoute);
+router.use('/async', docsRoute);
+
+module.exports = router;
