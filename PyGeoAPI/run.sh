dd9e9bf70fdea4d005bc04f59b9b95d7642361dc
pabloangusto <pangustod@gmail.com>
2025-05-25 19:51:19 +0200

    Add error handling for missing PGHOST argument in run script

--- PyGeoAPI/run.sh
+++ PyGeoAPI/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Uso: $0 <PGHOST>"
+  exit 1
+fi
+
+PGHOST="$1"
+
 docker run -p 5001:80 \
   --rm \
   --name pygeoapi_container \
+  -e PGHOST="$PGHOST" \
+  -v "$(pwd)/pygeoapi-config.yml:/pygeoapi/local.config.yml" \
   geopython/pygeoapi:latest
